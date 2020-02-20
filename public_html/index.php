d6349a346cc89ce1b656da9dd09603f99be7316b
Andy Reyes <andres.reyes@getbraintree.com>
2020-02-20 14:21:28 -0600

    quickfix

--- public_html/index.php
+++ public_html/index.php
@@ -28,5 +28,5 @@
   </div>
 </div>
     </body>
-<div style="text-align:center;padding:10px;margin-top:7px;"> {•̃̾_•̃̾} </div>
+<div style="text-align:center;padding:10px;margin-top:7px;"> <p>The Demos on this page use the following merchant ID: tt8srtpp8yfgfghp</p><p>{•̃̾_•̃̾}</p> </div>
 </html>
